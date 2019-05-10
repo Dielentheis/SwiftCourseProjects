342230ae8545d83c21a740ed54a344e49dcb6a7f
Elizabeth Dielentheis <elizabeth@scribd.com>
2019-05-10 15:51:46 -0700

    Loops

--- Loops.playground/Contents.swift
+++ Loops.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
 
-var str = "Hello, playground"
+func fibonacci(n : Int) {
+    var firstNum = 0
+    var secondNum = 1
+    var temp = 0
+    
+    if n == 0 {
+        print(firstNum)
+        return
+    } else if n == 1 {
+        print(secondNum)
+        return
+    }
+    
+    for _ in 1...(n - 2) {
+        temp = firstNum + secondNum
+        firstNum = secondNum
+        secondNum = temp
+        
+        print(temp)
+    }
+}
+
+fibonacci(n: 10)
