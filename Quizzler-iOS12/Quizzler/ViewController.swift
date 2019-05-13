a72792e81e05fe04def72166f762f601cf1df928
Elizabeth Dielentheis <elizabeth@scribd.com>
2019-05-13 16:46:10 -0700

    Quiz app

--- Quizzler-iOS12/Quizzler/ViewController.swift
+++ Quizzler-iOS12/Quizzler/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     
     let allQuestions = QuestionBank()
     var pickedAnswer : Bool = false
+    var currentQuestionIndex = 0
+    var score = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -21,8 +23,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let firstQuestion = allQuestions.list[0]
-        questionLabel.text = firstQuestion.questionText
+        updateUI()
     }
 
 
@@ -34,28 +35,51 @@ class ViewController: UIViewController {
         }
         
         checkAnswer()
+        nextQuestion()
     }
     
     
     func updateUI() {
-      
+        scoreLabel.text = "Score: \(score)"
+        questionLabel.text = allQuestions.list[currentQuestionIndex].questionText
+        progressLabel.text = "\(currentQuestionIndex + 1) / \(allQuestions.list.count)"
+        progressBar.frame.size.width = (view.frame.size.width / CGFloat(allQuestions.list.count)) * CGFloat(currentQuestionIndex + 1)
+        // update progress bar
     }
     
 
     func nextQuestion() {
-        
+        if currentQuestionIndex == allQuestions.list.count - 1 {
+            let alert = UIAlertController(title: "u done", message: "You finished! Wanna do it again?", preferredStyle: .alert)
+            let restartAction = UIAlertAction(title: "Restart", style: .default) { (UIAlertAction) in
+                self.startOver()
+            }
+            
+            alert.addAction(restartAction)
+            
+            present(alert, animated: true, completion: nil)
+        } else {
+            currentQuestionIndex += 1
+            updateUI()
+        }
     }
     
     
     func checkAnswer() {
+        let isCorrect = pickedAnswer == allQuestions.list[currentQuestionIndex].answer
         
+        if isCorrect {
+            score += 100
+            ProgressHUD.showSuccess("Correct!")
+        } else {
+            ProgressHUD.showError("Zoinks... no.")
+        }
     }
     
     
     func startOver() {
-       
+        currentQuestionIndex = 0
+        score = 0
+        updateUI()
     }
-    
-
-    
 }
